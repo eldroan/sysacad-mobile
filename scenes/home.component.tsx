3d7d03c909df6133b6312e4f1bb0d15c154dee2c
eldroan <leandroamarillo95@gmail.com>
2020-04-22 11:55:00 -0300

    Added web support

--- scenes/home.component.tsx
+++ scenes/home.component.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { Text } from "@ui-kitten/components";
+import { Text, Button } from "@ui-kitten/components";
 import {
   SafeAreaLayoutElement,
   SafeAreaLayout,
   SaveAreaInset,
 } from "../components/safe-are-layout.component";
+import { DefaultProps } from "../navigation/app.navigator";
 
-export const HomeScreen = (): SafeAreaLayoutElement => (
-  <SafeAreaLayout style={styles.safeArea} insets={SaveAreaInset.TOP}>
-    <Text appearance="hint" style={{ textAlign: "center" }}>
-      This is not the screen you are looking for...
-    </Text>
-  </SafeAreaLayout>
-);
+export const HomeScreen = ({
+  navigation,
+}: DefaultProps): SafeAreaLayoutElement => {
+  const goBack = () => {
+    navigation.pop()
+  };
+  return (
+    <SafeAreaLayout style={styles.safeArea} insets={SaveAreaInset.TOP}>
+      <Text appearance="hint" style={{ textAlign: "center", marginBottom:10 }}>
+        This is not the screen you are looking for...
+      </Text>
+      <Button onPress={goBack}>BACK</Button>
+    </SafeAreaLayout>
+  );
+};
 
 const styles = StyleSheet.create({
   safeArea: {
